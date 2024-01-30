ea5b8459a31488d56a42c5bba6ff4f1de9cbc6fd
Aaron Johnson <aaronjohnson@Pauls-MBP.lan>
2024-01-29 20:24:37 -0800

    12345 lab

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2; 
 
-// Write your code here
+
+let random = Math.floor(Math.random() * 1) + 1; 
+
+let num3 = 14;
+let num4 = 5;
+let mod = num3 % num4; 
+
+
+let setOfNumbers = [5, 10, 15, 20];
+let max = Math.max(...setOfNumbers); 
+
+console.log("multiply:", multiply);
+console.log("random:", random);
+console.log("mod:", mod);
+console.log("max:", max);
+
